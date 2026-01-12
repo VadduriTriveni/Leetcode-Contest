1224bcda10f4617889d8197c7e19d83321ddd5c9
Triveni <97962294+VadduriTriveni@users.noreply.github.com>
2026-01-12 20:07:08 +0530

    Update Q1.java

--- Weekly Contest/483/Q1.java
+++ Weekly Contest/483/Q1.java
@@ -1,1 +1,22 @@
+class Solution {
+    public String largestEven(String s) {
 
+        // Traverse from the end to find the last '2'
+        for (int i = s.length() - 1; i >= 0; i--) {
+
+            // Since only '1' and '2' exist, '2' is the only even digit
+            if (s.charAt(i) == '2') {
+
+                // Largest possible result is the prefix ending at this '2'
+                return s.substring(0, i + 1);
+            }
+        }
+
+        // No '2' found → no even number possible
+        return "";
+    }
+}
+
+Comments
+// Even numbers can be identified by their last digit
+// Do not initialize unnecessary variables
